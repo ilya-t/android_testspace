d0af8722e604cb3160e5851372662417cfab0636
Tskhay Ilya <extraletters@gmail.com>
2017-10-03 23:17:14 +0300

    upd NamedTrigger signature

--- mad-lab/src/main/java/com/madlab/core/NamedTrigger.java
+++ mad-lab/src/main/java/com/madlab/core/NamedTrigger.java
@@ -3,6 +3,14 @@ package com.madlab.core;
 /**
  * Version of trigger with caption.
  */
-public interface NamedTrigger extends Trigger {
-    String getCaption();
+public abstract class NamedTrigger implements Trigger {
+    private final String caption;
+
+    protected NamedTrigger(String caption) {
+        this.caption = caption;
+    }
+
+    protected final String getCaption() {
+        return caption;
+    }
 }
