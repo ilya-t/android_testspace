98d7cd6356d3d75ae1a5e8c5dd78ee7f50f00c9c
ilya-t <extraletters@gmail.com>
2023-12-04 12:29:15 +0300

    sample unit-test

---
+++ app/src/test/java/com/testspace/CurrentExperimentTest.kt
@@ -0,0 +1,11 @@
+package com.testspace
+
+import org.junit.Assert
+import org.junit.Test
+
+class CurrentExperimentTest {
+    @Test
+    fun smoke() {
+        Assert.assertEquals(true, true)
+    }
+}
